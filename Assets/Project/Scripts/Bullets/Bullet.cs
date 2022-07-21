a1f17b6f5e0e4a4294cf23a0e578018c630a046d
João Santos <ricardoelric@gmail.com>
2022-07-21 00:18:13 -0300

    imp: creating punch scale tween animation script to the bullet

--- Assets/Project/Scripts/Bullets/Bullet.cs
+++ Assets/Project/Scripts/Bullets/Bullet.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 using JoaoSant0s.ServicePackage.Pool;
 
 using AsteroidsGame.Bullets.Actions;
+using AsteroidsGame.Animations;
 
 namespace AsteroidsGame.Bullets
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(PunchScaleTweenAnimation))]
     public class Bullet : PoolBase
     {
         [SerializeField]
@@ -17,22 +18,36 @@ namespace AsteroidsGame.Bullets
 
         [SerializeField]
         private BulletContext context;
+
         private Rigidbody2D rb;
+        private PunchScaleTweenAnimation punchScaleTweenAnimation;
 
         #region Unity Methods
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
+            punchScaleTweenAnimation = GetComponent<PunchScaleTweenAnimation>();
         }
 
         #endregion
 
+        #region Protected Override Methods
+
+        protected override void OnShow()
+        {
+            punchScaleTweenAnimation.Run();
+        }
+
         protected override void OnDispose()
         {
             rb.velocity = Vector2.zero;
+            punchScaleTweenAnimation.CompleteTween();
+
             StopAllCoroutines();
         }
+
+        #endregion
 
         #region Public Methods
 
