2e0a666a57e67a826a26219bdde1cf05e05da851
João Santos <ricardoelric@gmail.com>
2022-07-30 17:00:58 -0300

    imp: updating personal packages versions
    - fixes in some classes based on packages updates

--- Assets/Project/Scripts/Common/UI/MainCanvas.cs
+++ Assets/Project/Scripts/Common/UI/MainCanvas.cs
@@ -11,13 +11,14 @@ namespace AsteroidsGame.UtilWrapper
     {
         [SerializeField]
         private Canvas canvas;
+        protected override bool IsDontDestroyOnLoad => false;
 
         public Vector2 Limits
         {
             get
             {
                 var canvasScale = canvas.transform.localScale;
-            
+
                 var rect = ((RectTransform)canvas.transform).rect;
 
                 var limits = new Vector2(rect.width * canvasScale.x / 2, rect.height * canvasScale.y / 2);
