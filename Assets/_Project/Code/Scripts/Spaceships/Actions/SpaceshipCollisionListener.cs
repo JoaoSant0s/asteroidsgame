3f2defefd5a46c9c3540ca2068d81b70abb6458b
João Santos <ricardoelric@gmail.com>
2024-02-21 00:18:26 -0300

    (wip): testing shader graphs

--- Assets/_Project/Code/Scripts/Spaceships/Actions/SpaceshipCollisionListener.cs
+++ Assets/_Project/Code/Scripts/Spaceships/Actions/SpaceshipCollisionListener.cs
@@ -9,7 +9,7 @@ namespace AsteroidsGame.Spaceships.Actions
 {
     [RequireComponent(typeof(Collider2D))]
     public class SpaceshipCollisionListener : MonoBehaviour
-    {        
+    {
         public static event Action AsteroidCollided;
 
         [Tag]
@@ -17,21 +17,50 @@ namespace AsteroidsGame.Spaceships.Actions
         private string asteroidTag;
 
         [SerializeField]
+        private SpriteRenderer spriteRender;
+
+        [SerializeField]
         private SpaceshipContext context;
+
+        private bool collided;
+        private float fade;
+        private MaterialPropertyBlock materialBlock;
 
         #region Unity Methods      
 
+        private void Awake()
+        {
+            materialBlock = new();
+        }
+
         void OnTriggerStay2D(Collider2D col)
         {
+            if (collided) return;
             if (!col.CompareTag(asteroidTag)) return;
             if (context.Invulnerable.Value) return;
+            collided = true;
 
-            Instantiate(context.Data.asteroidCollisionEffectPrefab, col.transform.position, Quaternion.identity);
-            RegisterSpaceshipCollision();
-            Destroy(gameObject);
+            StartCoroutine(SimulateDestroyFade(col.transform.position));
         }
 
         #endregion
+
+        private IEnumerator SimulateDestroyFade(Vector3 position)
+        {
+            fade = 0;
+            while (fade < 1)
+            {
+                fade += Time.deltaTime;
+                spriteRender.GetPropertyBlock(materialBlock);
+                materialBlock.SetFloat("_Fade", fade);
+                spriteRender.SetPropertyBlock(materialBlock);
+                yield return null;
+            }
+
+            Instantiate(context.Data.asteroidCollisionEffectPrefab, position, Quaternion.identity);
+            RegisterSpaceshipCollision();
+            Destroy(gameObject);
+        }
 
         private void RegisterSpaceshipCollision()
         {
